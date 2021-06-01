8905e2c67a737b23adfae3f9e81f6fb89984fbbc
Ale0x78 <anahape@ncsu.edu>
2021-06-01 00:31:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 try {
-fs.readdir('./', (err, files) => {
-  files.forEach(file => {
+fs.readdirSync('./').forEach(file => {
     console.log(file);
   });
-});
 } catch (error) {
   core.setFailed(error.message);
 }
