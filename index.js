1c0c1511b1bc3b3e23862f5e27266c1b3d8c1956
Alex <alex@Grishaverse.local>
2021-06-01 10:15:46 -0400

    Action update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
+// Is the trigger on pull request? 
+// Are there any secrets?
+// Are you using tags for versioning?? 
+// Are the actions you are using vulnrable? -- How...?
+
 try {
   console.log(fs.readdirSync("./");
 } catch (error) {
