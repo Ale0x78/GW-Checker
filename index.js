8fde418cad8c3bb55e5b30078541fc1802d9b2e0
Ale0x78 <anahape@ncsu.edu>
2021-06-01 00:26:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  console.log(`CWD: ${process.cwd()}`);
+fs.readdir('./', (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});
 } catch (error) {
   core.setFailed(error.message);
 }
